1980f007c35dee1843400c759cbc1b885c1f0111
abc3683 <abc3683@gmal.com>
2014-07-24 14:16:04 +0800

    wendy update

--- shiji/policy/indexers.py
+++ shiji/policy/indexers.py
@@ -1,7 +1,20 @@
 from plone.indexer.decorator import indexer
+from shiji.content.issue import IIssue
 from shiji.content.bulletin import IBulletin
 from shiji.content.diary import IDiary
 
+
+@indexer(IIssue)
+def category(obj):
+    return obj.category
+
+@indexer(IIssue)
+def origin(obj):
+    return obj.origin
+
+@indexer(IIssue)
+def track(obj):
+    return obj.track
 
 @indexer(IBulletin, IDiary)
 def date(obj):
